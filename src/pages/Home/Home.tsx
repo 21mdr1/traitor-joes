41d0f6977955d54cae1b7c596f988c180926906e
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-02-18 11:59:28 -0500

    Create basic home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,36 @@
+import { useState } from 'react';
 import './Home.scss';
 
+// type CalculatorProps = {
+//     left: number,
+//     operator: string,
+//     right: number,
+// }
+
 function Home() {
+    // let [ title, setTitle ] = useState<string>(null);
+
+    /* 
+    type RequestState =
+        | { status: 'idle' }
+        | { status: 'loading' }
+        | { status: 'success', data: any }
+        | { status: 'error', error: Error };
+
+    const [requestState, setRequestState] = useState<RequestState>({ status: 'idle' });
+    */
+    let [ name, setName ] = useState("");
+
     return (
         <>
+            <div>
+                <p>What's your name?</p>
+                <input />
+            </div>
+            <div>
+                <button>New Game</button>
+                <button>Join Game</button>
+            </div>
         </>
     );
 }
